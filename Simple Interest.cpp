3aab48dd90d98ff22d57c435160070bbdce50578
Shoaib Ahmed <shoaib30@gmail.com>
2014-09-07 20:36:35 +0530

    minor changes

--- Simple Interest.cpp
+++ Simple Interest.cpp
@@ -3,12 +3,12 @@ using namespace std;
 float sin(float p,int t,int r=8)
 {
     float in;
-    in=p*1*r/100;
+    in=p*t*r/100;
     return in;
 }
 int main()
 {
-    int i,t;
+    int t;
     float si,pr,tot;
     cout<<"Enter Principle : ";
     cin>>pr;
