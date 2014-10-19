df0fae54e6ad751ed4cbad87f926b2bf87a4f0f1
Shoaib Ahmed <shoaib30@gmail.com>
2014-10-19 16:07:36 +0530

    Distance
    Operator << >> overloading

---
+++ distance_overload_insertion_extraction.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class dist
+{
+    int feet,inch;
+public:
+    void display()
+    {
+        cout<<"Distance : "<<feet<<"'"<<inch<<"\"";
+    }
+    friend istream &operator >>(istream &din,dist &d1)
+    {
+        din>>d1.feet;
+        din>>d1.inch;
+    }
+    friend ostream &operator <<(ostream &dout,dist &d1)
+    {
+        dout<<d1.feet<<"'";
+        dout<<d1.inch<<"\"";
+    }
+};
+int main()
+{
+    dist d1;
+    cout<<"Enter Distance (feet inches)";
+    cin>>d1;
+    cout<<"\nDisplay:\nDistance :";
+    cout<<d1;
+    return 0;
+}
