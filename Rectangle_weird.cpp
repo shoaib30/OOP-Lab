f2ad47e9c22fa357741cad2cb15d60dd8319faef
Shoaib Ahmed <shoaib30@gmail.com>
2014-10-19 16:52:33 +0530

    rectangle
    overloading unary operator to change sign of rectangle

---
+++ Rectangle_weird.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class rectangle
+{
+    int l,b;
+public:
+    friend istream &operator >>(istream &din,rectangle &r)
+    {
+        din>>r.l;
+        din>>r.b;
+    }
+    friend ostream &operator <<(ostream &dout,rectangle &r)
+    {
+        dout<<"Lenght :"<<r.l<<endl;
+        dout<<"Breath :"<<r.b<<endl;
+    }
+    void operator -()
+    {
+        l*=-1;
+        b*=-1;
+    }
+};
+int main()
+{
+    rectangle r1,r2;
+    cout<<"Enter length and breath :";
+    cin>>r1;
+    r2=r1;
+    -r2;
+    cout<<"\nRectangles:\n";
+    cout<<r1<<endl<<r2;
+    return 0;
+}
